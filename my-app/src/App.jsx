199aca9696de9bcbeb2b33e73383dcf9a110a0a8
Roushni Sharma <roushni1104@gmail.com>
2025-06-30 07:12:27 +0000

    Initial

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+export const App = () => {
+    return (
+    <div>
+        <div>
+            <img src="C:\Users\hp\Downloads\r1.jpg" alt="photo hai" width={50} height={50} /> 
+        </div>
+        <h1>Name : Stranger Things</h1>
+        <h2>Rating : 8.1 </h2>
+        <p>yahan aayegi iski summary par mere paas toh hai hi nahi... </p>
+    </div>
+    )
+};
